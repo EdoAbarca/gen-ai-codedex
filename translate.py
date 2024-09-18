75159f30adbff44e66276883b60805245210c7ef
EdoAbarca <eduardo.abarca.c@usach.cl>
2024-09-17 22:36:09 -0300

    feat: Se implementan ejercicios de clasificación de texto, traducción, revisión de escritura y clasificación de opiniones.
    Se pretende cambiar archivos a .ipynb. Una segunda opción es escribir, por cada ejercicio, un archivo .md con los fundamentos.

---
+++ translate.py
@@ -0,0 +1,14 @@
+#pip install translate
+
+from translate import Translator
+
+translator = Translator(to_lang='es')  # Spanish
+
+# Text to be translated
+text = 'Hello, how are you?'
+
+# Perform the translation
+translation = translator.translate(text)
+
+# Print the translated text
+print('Translated Text:', translation)
